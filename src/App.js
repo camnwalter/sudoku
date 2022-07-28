c9f910387e9789858828cb9e31ae00f36f884b92
Cam Walter <cam.n.walter@gmail.com>
2022-07-28 17:26:36 -0500

    Add basic functionality: can now type in boxes :)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import { useState } from "react";
+import { Cell } from "./components/Cell";
 import "./App.css";
-import { Board } from "./components/Board";
 
 const BOARD = Array(9)
   .fill(null)
-  .map((arr) => Array(9).fill(null));
+  .map(() => Array(9).fill(null));
 
-function App() {
+export const App = () => {
+  const [selected, setSelected] = useState(-1);
+
+  const isAdjacent = (id) => {
+    return (
+      id % 9 === selected % 9 || Math.floor(id / 9) === Math.floor(selected / 9)
+    );
+  };
+
+  const inSame3x3 = (id) => {
+    const x = Math.floor(id / 9);
+    const y = Math.floor(id % 9);
+
+    const selectedX = Math.floor(selected / 9);
+    const selectedY = Math.floor(selected % 9);
+    return (
+      Math.floor(x / 3) === Math.floor(selectedX / 3) &&
+      Math.floor(y / 3) === Math.floor(selectedY / 3)
+    );
+  };
+
   return (
     <div className="App">
-      <Board elements={BOARD}></Board>
+      {BOARD.map((column, col) => (
+        <div id="col">
+          {column.map((cell, row) => {
+            const id = col * 9 + row;
+            return (
+              <Cell
+                id={id}
+                selected={id === selected}
+                adjacent={isAdjacent(id) || inSame3x3(id)}
+                onClick={(e) => {
+                  setSelected(id);
+                }}
+              ></Cell>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
-}
-
-export default App;
+};
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,15 +1,36 @@
 import { useState } from "react";
 
-export const Cell = ({ selected, adjacent, onClick }) => {
-  const [number, setNumber] = useState(-1);
-
-  const isValidNumber = (number) => number >= 1 && number <= 9;
+export const Cell = ({ adjacent, id, onClick, selected }) => {
+  const [text, setText] = useState("");
+  const [cornerNotes, setCornerNotes] = useState([]);
 
   return (
     <div
       className="cell"
-      id={selected ? "selected" : adjacent ? "adjacent" : ""}
+      id={selected ? "focused" : adjacent ? "adjacent" : ""}
+      tabIndex={-1}
+      onKeyDown={(e) => {
+        e.preventDefault();
+        if (e.key === "Backspace") {
+          setText("");
+        } else if (e.key === "Delete") {
+          setText("");
+          setCornerNotes([]);
+        } else if (e.key <= 9 && e.key >= 1) {
+          if (e.ctrlKey) {
+            if (cornerNotes.includes(e.key)) {
+              setCornerNotes((notes) => notes.filter((note) => note !== e.key));
+            } else {
+              setCornerNotes((notes) => notes.concat(e.key));
+            }
+          } else {
+            setText(e.key);
+          }
+        }
+      }}
       onClick={onClick}
-    ></div>
+    >
+      <div className="centerText">{text}</div>
+    </div>
   );
 };

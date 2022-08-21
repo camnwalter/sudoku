f97d5d8569abd3fdaa0bb4d2b6bf684337a5338a
Cam Walter <cam.n.walter@gmail.com>
2022-08-21 15:38:02 -0500

    Add 404 page

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { App } from "./App";
+import { NotFound } from "./components/NotFound";
 import { Environment } from "./utils/utils";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App environment={Environment.Basic} />} />
       <Route
@@ -18,6 +19,7 @@ root.render(
         path="/play/:id"
         element={<App environment={Environment.Play} />}
       />
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );

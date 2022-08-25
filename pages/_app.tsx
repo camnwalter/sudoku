d6fda57b88b3843026b2a5ce12ae0601dfd90256
Cam Walter <cam.n.walter@gmail.com>
2022-08-24 21:51:26 -0500

    Fix head once and for all?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,34 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SudokuProvider>
-      <UndoRedoProvider>
-        <TimeProvider>
-          <Header />
-          <Component {...pageProps} />
-        </TimeProvider>
-      </UndoRedoProvider>
-    </SudokuProvider>
+    <>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/static/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:title" content="Sudoku" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://squagward.com" />
+      <meta
+        property="og:image"
+        content="https://github.com/Squagward/sudoku/raw/master/public/logo512.png"
+      />
+      <meta
+        property="og:description"
+        content="Classic sudoku inspired by Cracking the Cryptic"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <link rel="apple-touch-icon" href="/logo192.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <title>Sudoku</title>
+      <SudokuProvider>
+        <UndoRedoProvider>
+          <TimeProvider>
+            <Header />
+            <Component {...pageProps} />
+          </TimeProvider>
+        </UndoRedoProvider>
+      </SudokuProvider>
+    </>
   );
 }
 

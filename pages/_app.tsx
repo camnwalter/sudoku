d684067cd4a5e0c25676073e8e7da28e74fc3882
Cam Walter <cam.n.walter@gmail.com>
2022-09-24 20:59:46 -0500

    add all sorts of icons, not sure if this will help

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <meta charSet="utf-8" />
-      <link rel="icon" href="/static/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta property="og:title" content="Sudoku" />
       <meta property="og:type" content="website" />
@@ -22,8 +21,27 @@ function MyApp({ Component, pageProps }: AppProps) {
         property="og:description"
         content="Classic sudoku inspired by Cracking the Cryptic"
       />
-      <link rel="apple-touch-icon" href="/logo192.png" />
-      <link rel="manifest" href="/manifest.json" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
       <title>Sudoku</title>
       <SudokuProvider>
         <UndoRedoProvider>
